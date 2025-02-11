19e8c5fd075933fe32ee9a19cac8b1d646e97453
Greysen Roy-Pascoe <gr10689@email.vccs.edu>
2025-02-10 19:10:03 -0500

    Added a bit more comment

--- m1 review project.cpp
+++ m1 review project.cpp
@@ -1,5 +1,6 @@
 // Main branch
 // This branch will be the bare bones of the next 2 programs
+//      One will be the RPS sim and the grade book will be the other
 // Greysen Roy-Pascoe
 // 1/29/25
 
